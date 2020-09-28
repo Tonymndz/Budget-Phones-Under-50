865972393504296ce72acab48282e62b1a93610b
Tonymndz <Tonymndz1141@gmail.com>
2020-09-28 14:29:32 -0700

    Updates

---
+++ precache-manifest.d083312899dc7a6df340915107107a86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11062416287835a88630fc522e22c57",
+    "url": "/BestBudgetPhonesUnder50/index.html"
+  },
+  {
+    "revision": "4de418bdd8c7f51f902b",
+    "url": "/BestBudgetPhonesUnder50/static/css/main.927e9701.chunk.css"
+  },
+  {
+    "revision": "4c35f6e8acd0dd03f391",
+    "url": "/BestBudgetPhonesUnder50/static/js/2.84139949.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BestBudgetPhonesUnder50/static/js/2.84139949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de418bdd8c7f51f902b",
+    "url": "/BestBudgetPhonesUnder50/static/js/main.e48509b3.chunk.js"
+  },
+  {
+    "revision": "089e07060f2fadc0cf4d",
+    "url": "/BestBudgetPhonesUnder50/static/js/runtime-main.87802019.js"
+  },
+  {
+    "revision": "0de5a24beb2e09c88fa289bc9acf53c6",
+    "url": "/BestBudgetPhonesUnder50/static/media/SG_S4.0de5a24b.JPG"
+  },
+  {
+    "revision": "8860b07ee44e49c0a877dc0635e2fd9e",
+    "url": "/BestBudgetPhonesUnder50/static/media/pileOfPhones.8860b07e.jpg"
+  }
+]);
